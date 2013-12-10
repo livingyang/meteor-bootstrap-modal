04f6ef615f35e558a5cc4cdd9c8fe3f35045e5e7
livingyang <lhm.cold@163.com>
2013-12-10 17:07:49 +0800

    Update: 2.2.1

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  summary: "Provides bootstrap modal v2.2.0"
+  summary: "Provides bootstrap modal v2.2.1"
 });
 
 Package.on_use(function (api) {
   api.use('bootstrap-3', 'client');
 
   var path = Npm.require('path');
-  var asset_path = path.join('bootstrap-mocal');
+  var asset_path = path.join('bootstrap-modal');
 
   // css
   api.add_files(path.join(asset_path, 'css', 'bootstrap-modal-bs3patch.css'), 'client');
